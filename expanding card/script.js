83a521911f7a20881de46412e5e436f7c22b4828
sahdilber <dilber-sah@hotmail.com>
2023-05-21 16:14:47 +0300

    expanding card

---
+++ expanding card/script.js
@@ -0,0 +1,17 @@
+//amacımız sadece onckilck olduğunda classı değiştirmek
+
+const panels=document.querySelectorAll('.panel')
+
+panels.forEach((panel)=>{
+    panel.addEventListener('click',()=>{
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+
+})
+
+function removeActiveClasses(){
+    panels.forEach(panel=>{
+        panel.classList.remove('active')
+    })
+}
